17a0931c918e7e48eb486d8225df933d69a8c45a
borisrosedev <boris.rose.dev@gmail.com>
2025-02-13 21:11:01 +0100

    feat/v1

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
+
+app.get('/', (req,res) => {
+    return res.status(200).json('Hello les amis')
+})
+
+app.post('/register', (req, res) => {
+    const { email, password, confirmedPassword } = req.body
+    if(email){
+        console.log('email', email)
+    } 
+    if(password){
+        console.log('password', password)
+    }
+    if(confirmedPassword){
+        console.log('confirmedPassword', confirmedPassword)
+    }
+
+    return res.json({ message: 'Félicitations vous êtes bien inscrit(e). Nous avons hâte de faire votre connaissance'})
+
+})
+
+
+app.listen(3000, () => {
+    console.log('✅ Server running at port 3000')
+})
+
+

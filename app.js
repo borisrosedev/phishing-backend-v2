2e5c400ab7059f284a40308240e87c7205cf0c54
Boris Dev <131349729+borisrosedev@users.noreply.github.com>
2025-05-07 19:31:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/', (req,res) => {
 app.post('/wed', (req, res) => {
     console.log(req.body)
     return res.status(200).json({data: req.body})
-}
+})
 
 app.post('/register', (req, res) => {
     const { email, password, confirmedPassword } = req.body
